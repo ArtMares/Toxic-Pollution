a180cc000effe6e88ae5afa947c13c2b6f015425
Dmitriy Dergachev <dergachevdv90@gmail.com>
2018-11-07 23:35:56 +0300

    Replace symbol \t to 4 space

--- data/items.lua
+++ data/items.lua
@@ -1,49 +1,49 @@
 data:extend({
-	{
-		type = "armor",
-		name = "respirator",
-		icon = "__deadlyPollution__/graphics/respirator.png",
-		icon_size = 64,
-		flags = {"goes-to-main-inventory"},
-		resistances = {
-			{type = "physical",		decrease = 0, percent = 0},
-			{type = "acid",			decrease = 0, percent = 0},
-			{type = "explosion",	decrease = 0, percent = 0},
-			{type = "fire",			decrease = 0, percent = 0}
-		},
-		durability = 500,
-		subgroup = "armor",
-		order = "f[hazard]",
-		stack_size = 10
-	},
-	{
-		type = "recipe",
-		name = "respirator",
-		enabled = true,
-		ingredients = {
-			{"iron-plate", 2},
-			{"wood", 1},
-			{"coal", 2}},
-		result = "respirator"
-	},
-	-- For upgrade technology
-	{
-		type = "item",
-		name = "clock-dummy",
-		icon = "__deadlyPollution__/graphics/clock.png",
-		icon_size = 64,
-		flags = { "goes-to-main-inventory" },
-		subgroup = "raw-material",
-		order = "g[plastic-bar]-h[unused-air-filter]",
-		stack_size = 50
-	},
-	{
-		type = "recipe",
-		name = "clock-dummy",
-		enabled = false,
-		ingredients = {},
-		result = "clock-dummy",
-		energy_required = 0.2,
-		hide = true
-	},
+    {
+        type = "armor",
+        name = "respirator",
+        icon = "__deadlyPollution__/graphics/respirator.png",
+        icon_size = 64,
+        flags = {"goes-to-main-inventory"},
+        resistances = {
+            {type = "physical",     decrease = 0, percent = 0},
+            {type = "acid",         decrease = 0, percent = 0},
+            {type = "explosion",    decrease = 0, percent = 0},
+            {type = "fire",         decrease = 0, percent = 0}
+        },
+        durability = 500,
+        subgroup = "armor",
+        order = "f[hazard]",
+        stack_size = 10
+    },
+    {
+        type = "recipe",
+        name = "respirator",
+        enabled = true,
+        ingredients = {
+            {"iron-plate", 2},
+            {"wood", 1},
+            {"coal", 2}},
+        result = "respirator"
+    },
+    -- For upgrade technology
+    {
+        type = "item",
+        name = "clock-dummy",
+        icon = "__deadlyPollution__/graphics/clock.png",
+        icon_size = 64,
+        flags = { "goes-to-main-inventory" },
+        subgroup = "raw-material",
+        order = "g[plastic-bar]-h[unused-air-filter]",
+        stack_size = 50
+    },
+    {
+        type = "recipe",
+        name = "clock-dummy",
+        enabled = false,
+        ingredients = {},
+        result = "clock-dummy",
+        energy_required = 0.2,
+        hide = true
+    },
 })
--- data/tech.lua
+++ data/tech.lua
@@ -1,81 +1,81 @@
 local function getIngridients(num)
-	local res = {}
-	local c = math.floor(num / 5)
-	table.insert(res, {"science-pack-1", 1})
-	if (c > 0) then
-		table.insert(res, {"science-pack-2", 1})
-	end
-	if (c > 1) then
-		table.insert(res, {"military-science-pack", 1})
-	end
-	if (c > 2) then
-		table.insert(res, {"science-pack-3", 1})
-	end
-	if (c > 3) then
-		table.insert(res, {"high-tech-science-pack", 1})
-	end
-	return res
+    local res = {}
+    local c = math.floor(num / 5)
+    table.insert(res, {"science-pack-1", 1})
+    if (c > 0) then
+        table.insert(res, {"science-pack-2", 1})
+    end
+    if (c > 1) then
+        table.insert(res, {"military-science-pack", 1})
+    end
+    if (c > 2) then
+        table.insert(res, {"science-pack-3", 1})
+    end
+    if (c > 3) then
+        table.insert(res, {"high-tech-science-pack", 1})
+    end
+    return res
 end
 
 data:extend({
-	{
-		type = "technology",
-		name = "respiration-lifeTime-1",
-		icon = "__deadlyPollution__/graphics/gas-time-research.png",
-		icon_size = 128,
-		effects = {
-			{type = "unlock-recipe", recipe = "clock-dummy"}
-		},
-		unit = {
-			count = 10,
-			ingredients = getIngridients(1),
-			time = 5
-		},
-		upgrade = true,
-		order = "c-k-f-e"
-	}
+    {
+        type = "technology",
+        name = "respiration-lifeTime-1",
+        icon = "__deadlyPollution__/graphics/gas-time-research.png",
+        icon_size = 128,
+        effects = {
+            {type = "unlock-recipe", recipe = "clock-dummy"}
+        },
+        unit = {
+            count = 10,
+            ingredients = getIngridients(1),
+            time = 5
+        },
+        upgrade = true,
+        order = "c-k-f-e"
+    }
 })
 
 for i = 2, 25 do
-	data:extend({
-		{
-			type = "technology",
-			name = "respiration-lifeTime-"..i,
-			icon = "__deadlyPollution__/graphics/gas-time-research.png",
-			icon_size = 128,
-			effects = {
-				{type = "unlock-recipe", recipe = "clock-dummy"}
-			},
-			prerequisites = {"respiration-lifeTime-"..(i - 1)},
-			unit = {
-				count = i*10,
-				ingredients = getIngridients(i),
-				time = i*5
-			},
-			upgrade = true,
-			order = "c-k-f-e"
-		}
-	})
+    data:extend({
+        {
+            type = "technology",
+            name = "respiration-lifeTime-"..i,
+            icon = "__deadlyPollution__/graphics/gas-time-research.png",
+            icon_size = 128,
+            effects = {
+                {type = "unlock-recipe", recipe = "clock-dummy"}
+            },
+            prerequisites = {"respiration-lifeTime-"..(i - 1)},
+            unit = {
+                count = i*10,
+                ingredients = getIngridients(i),
+                time = i*5
+            },
+            upgrade = true,
+            order = "c-k-f-e"
+        }
+    })
 end
 
 -- 
 --[[
 data:extend({
-	{
-		type = "technology",
-		name = "respiration-lifeTime-"..i,
-		icon = "__deadlyPollution__/graphics/gas-time-research.png",
-		icon_size = 128,
-		effects = {
-			{type = "unlock-recipe", recipe = "clock-dummy"}
-		},
-		unit = {
-			count = i*10,
-			ingredients = getIngridients(i),
-			time = i*5
-		},
-		upgrade = true,
-		order = "c-k-f-e"
-	}
+    {
+        type = "technology",
+        name = "respiration-lifeTime-"..i,
+        icon = "__deadlyPollution__/graphics/gas-time-research.png",
+        icon_size = 128,
+        effects = {
+            {type = "unlock-recipe", recipe = "clock-dummy"}
+        },
+        unit = {
+            count = i*10,
+            ingredients = getIngridients(i),
+            time = i*5
+        },
+        upgrade = true,
+        order = "c-k-f-e"
+    }
 })
 ]]
