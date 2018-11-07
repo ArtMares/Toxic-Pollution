d81a52a40715404fa606123e2ffbdac0a9f27fa2
Dmitriy Dergachev <dergachevdv90@gmail.com>
2018-11-07 23:35:27 +0300

    Add auto-equip-armor to settings

--- settings.lua
+++ settings.lua
@@ -1,20 +1,26 @@
 data:extend(
 {
-	{
-		type = "int-setting",
-		name = "armor-absorb-multiplicator",
-		setting_type = "startup",
-		default_value = 5000,
-		maximum_value = 100000,
-		minimum_value = 100
-	},
-	{
-		type = "double-setting",
-		name = "min-pollution-to-damage",
-		setting_type = "startup",
-		default_value = 200,
-		maximum_value = 1000,
-		minimum_value = 100
-	}
+    {
+        type = "int-setting",
+        name = "armor-absorb-multiplicator",
+        setting_type = "startup",
+        default_value = 5000,
+        maximum_value = 100000,
+        minimum_value = 100
+    },
+    {
+        type = "double-setting",
+        name = "min-pollution-to-damage",
+        setting_type = "startup",
+        default_value = 200,
+        maximum_value = 1000,
+        minimum_value = 100
+    },
+    {
+        type = "bool-setting",
+        name = "auto-equip-armor",
+        setting_type = "startup",
+        default_value = true
+    }
 }
 )
